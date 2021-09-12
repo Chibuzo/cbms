f74713f2da740ec8bdee0aedc19b63247a0bf61e
Chibuzo <chibuzo.henry@gmail.com>
2021-09-12 23:30:52 +0100

    Add date filtering

--- routes/employee.js
+++ routes/employee.js
@@ -3,7 +3,8 @@ const { fetchEmployees } = require('../services/employeeService');
 
 routes.get('/', async (req, res, next) => {
     try {
-        const { employees, metadata } = await fetchEmployees(parseInt(req.query.page || 1, 10));
+        const { page, month, year } = req.query;
+        const { employees, metadata } = await fetchEmployees(parseInt(page || 1, 10), month, year);
         res.status(200).json({ status: true, data: { employees, metadata } });
     } catch (err) {
         console.log(err)
--- services/employeeService.js
+++ services/employeeService.js
@@ -1,12 +1,28 @@
 const { getConnection } = require('../config/dbconnection');
 const { ErrorHandler } = require('../helpers/errorHandler');
 
-const fetchEmployees = async (page = 1) => {
+const fetchEmployees = async (page = 1, month, year) => {
     if (!Number.isInteger(page) || page < 1) {
         throw new ErrorHandler(400, 'Invalid page. Page must be a valid integer, greater 0');
     }
+
+    const query_params = [];
+    let date_query = '';
+
+    if (month) {
+        const MONTH = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+        if (!MONTH.includes(month.toUpperCase())) throw new ErrorHandler(400, 'Invalid month. Please use a 3 character prefix for months. Eg JAN');
+        date_query += "AND to_char(pay.effective_date, 'MON') = :month ";
+        query_params.push(month.toUpperCase());
+    }
+    if (year) {
+        if (year.length !== 2) throw new ErrorHandler(400, 'Invalid year. Please use a 2 character year. Eg 21');
+        date_query += "AND to_char(pay.effective_date, 'YY') = :year ";
+        query_params.push(year);
+    }
     const limit = 100;
     const offset = page === 1 ? 1 : (page - 1) * limit;
+    query_params.push(offset, limit);
 
     const query = `
         select pay.full_name, per.sex "Gender", per.national_identifier "ID No", per.employee_number, ass.grade_id "Grade Code", grd.name "Grade Name",
@@ -30,13 +46,13 @@ const fetchEmployees = async (page = 1) => {
         join pay_element_types_f ele on (ele.element_type_id=pay.element_type_id)
         join per_grades grd on (grd.grade_id = ass.grade_id)
         join per_jobs job on (job.job_id = ass.job_id)
-        where to_char(pay.effective_date, 'MON')='JUL' and pay.segment10 is not null 
+        where pay.segment10 is not null ${date_query}
         OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY`;
 
     const db = await getConnection();
     const [result, total_count] = await Promise.all([
-        db.execute(query, [offset, limit]),
-        countRecords()
+        db.execute(query, query_params),
+        countRecords(date_query, month, year)
     ]);
     const employees = result.rows;
 
@@ -53,17 +69,21 @@ const fetchEmployees = async (page = 1) => {
     return { employees, metadata };
 };
 
-const countRecords = async () => {
+const countRecords = async (date_query, month, year) => {
     const query = `
         select COUNT(*) num
         FROM pay_costing_details_v pay
         join per_all_people_f per on (pay.person_id = per.person_id)
         join per_all_assignments_f ass on (ass.assignment_id=pay.assignment_id)
         join pay_element_types_f ele on (ele.element_type_id=pay.element_type_id)
-        where to_char(pay.effective_date, 'MON')='JUL' and pay.segment10 is not null`;
+        where pay.segment10 is not null ${date_query}`;
+
+    const query_params = [];
+    if (month) query_params.push(month.toUpperCase());
+    if (year) query_params.push(year);
 
     const db = await getConnection();
-    const result = await db.execute(query);
+    const result = await db.execute(query, query_params);
     const count = result.rows[0];
     return count.NUM;
 }
