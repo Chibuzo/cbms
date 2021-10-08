6fd36193d70e723e1fa4a408070264164a413851
Chibuzo <chibuzo.henry@gmail.com>
2021-10-08 16:24:56 +0100

    Rename field

--- services/employeeService.js
+++ services/employeeService.js
@@ -26,7 +26,7 @@ const fetchEmployees = async (page = 1, month, year) => {
 
     const query = `
         select pay.full_name, per.sex "GENDER", per.national_identifier "ID_NO", per.employee_number, ass.grade_id "GRADE_CODE", grd.name "GRADE_NAME",
-        spi.sequence "GRADESTEP/NOTCH",
+        spi.sequence "NOTCH",
         ele.element_type_id "PAY_CODE", ele.element_name "PAYCODE_NAME",ele.attribute1 "COST_CODE",
         CASE WHEN pay.debit_amount = 0 THEN pay.credit_amount ELSE pay.debit_amount END AS AMOUNT,
         to_char(ass.effective_start_date, 'DD-MON') || '-' || extract(year from sysdate) "PROMOTION_DATE",
